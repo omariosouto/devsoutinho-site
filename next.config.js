0c6b5fbf8cf0e6963807e30997c38ee89d5c59f9
Mario Souto <soutomarios@gmail.com>
2020-08-31 21:46:05 -0300

    important adjusts

---
+++ next.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // 1) https://nextjs.org/docs/api-reference/next.config.js/trailing-slash
+  // https://webmasters.googleblog.com/2010/04/to-slash-or-not-to-slash.html
+  trailingSlash: true,
+  // 2) https://nextjs.org/docs/api-reference/next.config.js/redirects
+  // https://moz.com/learn/seo/redirection
+  async redirects() {
+    return [
+      {
+        source: '/redirect-404',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+  // 3) https://nextjs.org/docs/api-reference/next.config.js/headers
+  // https://gtmetrix.com/enable-gzip-compression.html#:~:text=The%20reason%20gzip%2FBrotli%20works,by%20up%20to%2070%25!
+  // https://javascript.info/clickjacking
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          // {
+          //   key: 'Content-Encoding',
+          //   value: 'gzip',
+          // },
+          {
+            key: 'x-frame-options',
+            value: 'DENY',
+          }
+        ],
+      }
+    ]
+  }
+}
