38861651d0c6435c9117090af06adb784df67162
Mario Souto <soutomarios@gmail.com>
2020-09-07 17:56:36 -0300

    testing

---
+++ pages/api/bots/profile-picker.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+ 
+
+export default async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.goto('https://www.instagram.com/omariosouto/');
+  const instagramProfilePic = await page.evaluate(() => {
+    return document.querySelector('[data-testid="user-avatar"]').src;
+  });
+  
+  await browser.close();
+
+  res.send({
+    pics: [
+      instagramProfilePic,
+    ]
+  });
+}
