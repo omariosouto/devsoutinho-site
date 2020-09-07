9d0a957e2f32fa2295f7bf8c010d7f9c6e151f97
Mario Souto <soutomarios@gmail.com>
2020-09-07 20:40:03 -0300

    m

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div>
       <Head title="Home - DevSoutinho Site" />
       <Header />
-      <a href="http://localhost:3000/api/graphql/"></a>
+      <a href="/api/graphql/">aaaaaa</a>
       <main>
         <Typography>
           Conteúdo unico de cada página
