73e181bc12f893a536ae22a82c5c4ce0e67d658f
Mario Souto <soutomarios@gmail.com>
2020-09-07 21:15:01 -0300

    test puppeteer

---
+++ pages/api/bot.js
@@ -0,0 +1,28 @@
+import chromium from 'chrome-aws-lambda';
+
+export default async (req, res) => {
+  const browser = await chromium.puppeteer.launch({
+    // Required
+    executablePath: await chromium.executablePath,
+    // // Optional
+    // args: chromium.args,
+    // defaultViewport: chromium.defaultViewport,
+    // headless: chromium.headless
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto('https://www.instagram.com/omariosouto/');
+
+  const instagramProfilePic = await page.evaluate(() => {
+    return document.querySelector('[data-testid="user-avatar"]').src;
+  });
+
+  await browser.close();
+
+  res.send({
+    pics: [
+      instagramProfilePic,
+    ]
+  });
+}
