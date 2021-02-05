972511d05bbdd53a05a9a10ae925b1f4d50b0dcf
Gabriel Nunes <gnunesmail@gmail.com>
2021-02-05 20:27:56 -0300

    Adicionado analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,17 @@ import App from 'next/app'
 
 import './styles.css'
 export default class MyApp extends App {
+
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "//gc.zgo.at/count.js";
+    script.async = true;
+    script.dataset.goatcounter = "https://gnunes.goatcounter.com/count"
+
+    document.body.appendChild(script);
+  }
+
   render() {
     const { Component, pageProps } = this.props
     return (
